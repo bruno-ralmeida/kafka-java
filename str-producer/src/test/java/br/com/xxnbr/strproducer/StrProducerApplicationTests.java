62c075c0b94aeb55684be09f9cdb108dac4c3b40
Bruno Almeida <bruno-ralmeida@outlook.com>
2023-01-12 13:36:55 -0300

    :tada: init: Criação do projeto

---
+++ str-producer/src/test/java/br/com/xxnbr/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xxnbr.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
