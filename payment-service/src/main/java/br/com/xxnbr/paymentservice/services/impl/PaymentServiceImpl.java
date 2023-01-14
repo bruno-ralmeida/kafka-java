829f391246887c72a6d9ec116ed6083c497e684d
Bruno Almeida <bruno-ralmeida@outlook.com>
2023-01-14 19:30:31 -0300

    :sparkles: feat: Enviando pagamento para o kafka

--- payment-service/src/main/java/br/com/xxnbr/paymentservice/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/br/com/xxnbr/paymentservice/services/impl/PaymentServiceImpl.java
@@ -2,14 +2,30 @@ package br.com.xxnbr.paymentservice.services.impl;
 
 import br.com.xxnbr.paymentservice.models.Payment;
 import br.com.xxnbr.paymentservice.services.PaymentService;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    private KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: RECEBI O PAGAMENTO {}", payment.toString());
+        log.info("RECEBI O PAGAMENTO {}", payment.toString());
+
+        Thread.sleep(1000);
+
+        log.info("ENVIANDO PAGAMENTO...");
+
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
